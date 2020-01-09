7ae0e44b6fbdc7436e5eb7cdef99ee2006cfc35e
unknown <kryptech.dev@gmail.com>
2020-01-09 12:07:49 +1100

    Debugging

--- database.py
+++ database.py
@@ -5,6 +5,7 @@ import os
 # If having trouble writing BLOB (BYTEA) data into Postgres try encoding it into a string with base64 and then store as text
 
 connection_pool = psycopg2.pool.ThreadedConnectionPool(5, 20, os.environ["DATABASE"])
+print(os.environ)
 print(os.environ["DATABASE"])
 def get_conn(cursor=True):
     conn = connection_pool.getconn()
