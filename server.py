2d9107f4957b8c5fd7625e0e227572c5320133e1
unknown <kryptech.dev@gmail.com>
2020-01-09 12:38:17 +1100

    Generating a random secret key

--- server.py
+++ server.py
@@ -1,12 +1,18 @@
 from flask import Flask
 
 import database as db
-
 from auth import auth
+
+import string
+import random
 
 app = Flask(__name__)
 
 app.register_blueprint(auth)
+
+secret_key = "".join(random.choice(string.printable) for _ in range(32))
+app.secret_key = secret_key
+print(f"Booting up with secret key {secret_key}")
 
 @app.route("/ping")
 def ping():
