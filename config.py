c88c8ee8ae11430f4c847638dc6d488abfdda4cb
unknown <kryptech.dev@gmail.com>
2020-01-11 10:05:10 +1100

    Updated client secret again

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CLIENT_ID = 664464975934390272
-CLIENT_SECRET = "dkCyVtlyc82cmBT4t4vYcPVf65LK1X3z"
+CLIENT_SECRET = "dkCyVtlyc82cmBT4t4vYcPVf65LK1X3Z"
 MY_URL = "http://odesaw.herokuapp.com"
 AUTHORIZE_URL = "https://discordapp.com/api/oauth2/authorize"
 TOKEN_URL = "https://discordapp.com/api/oauth2/token"
