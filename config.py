c7c506f39f75ec46df6c1960dc04a5d4716c30a2
unknown <kryptech.dev@gmail.com>
2020-01-11 11:19:17 +1100

    Another key

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 CLIENT_ID = 664464975934390272
-CLIENT_SECRET = "dkCyVtlyc82cmBT4t4vYcPVf65LKlX3Z"
+CLIENT_SECRET = "dkCyVt1yc82cmBT4t4vYcPVf65LK1X3Z"
 MY_URL = "http://odesaw.herokuapp.com"
 AUTHORIZE_URL = "https://discordapp.com/api/oauth2/authorize"
 TOKEN_URL = "https://discordapp.com/api/oauth2/token"
